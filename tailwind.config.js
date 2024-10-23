b5bb0e68c1151e328bd99364cf7a7688faf326e3
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-10-22 21:31:57 -0600

    Partially add tailwind, but doesn't quite work
    The UI will not automatically load the built css.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/io/dbme/frontend/views/**/*.cljs",
+            "./resources/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
